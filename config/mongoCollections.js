c778037fa8a4c3bd39dd2e7c185ad7be282932ed
Aditya Munot <munot4@gmail.com>
2019-11-28 07:15:36 -0500

    Initial Setup

---
+++ config/mongoCollections.js
@@ -0,0 +1,15 @@
+const dbConnection = require("./mongoConnection");
+const getCollection = collection =>{
+  let _col = undefined;
+  return async()=>{
+    if(!_col){
+      const db = await dbConnection();
+      _col     = await db.collection(collection);
+    }
+    return _col;
+  };
+};
+
+module.exports={
+  
+}
