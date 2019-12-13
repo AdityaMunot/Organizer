861b1d90daac8fe609bcf338629910672d981847
Aditya Munot <munot4@gmail.com>
2019-12-13 17:52:20 -0500

    Basic structure layout

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
-
-
-
+// const bodyParser = require("body-parser");
 const configRoutes = require("./routes");
+const expHbs = require("express-handlebars");
+const static = express.static(__dirname + "/public");
 
 const app = express();
+// app.use(bodyParser.urlencoded());
 
+app.use("/public", static)
+app.engine("handlebars", expHbs({defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 configRoutes(app);
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 
-
 const constructorMethod = app => {
-    
+    app.use("/", (req, res) => {
+        try {
+            res.render("details/home")
+        } catch (e) {
+            res.status(404).json({ error: e })
+        }
+    })
+
+    app.use("*",(req,res)=>{
+        res.status(404).render("details/error",{error:{status:404,message:"Not found"}});
+        }
+    )
 }
 
 module.exports = constructorMethod
