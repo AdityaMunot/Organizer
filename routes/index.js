51d5276e4f56ae0b231d67ce66b892ae63ec03dd
Aditya Munot <munot4@gmail.com>
2019-12-15 02:08:32 -0500

    Added views

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 
 const constructorMethod = app => {
-    app.use("/", (req, res) => {
+
+    app.use("/home", (req, res) => {
         try {
             res.render("details/home")
         } catch (e) {
@@ -8,10 +9,41 @@ const constructorMethod = app => {
         }
     })
 
-    app.use("*",(req,res)=>{
-        res.status(404).render("details/error",{error:{status:404,message:"Not found"}});
+    app.use("/signup", (req, res) => {
+        try {
+            res.render("details/signup")
+        } catch (e) {
+            res.status(404).json({ error: e })
         }
-    )
+    })
+
+    app.use("/signin", (req, res) => {
+        try {
+            res.render("details/signin")
+        } catch (e) {
+            res.status(404).json({ error: e })
+        }
+    })
+
+    app.use("/schedule", (req, res) => {
+        try {
+            res.render("details/schedule")
+        } catch (e) {
+            res.status(404).json({ error: e })
+        }
+    })
+
+    app.use("/finance", (req, res) => {
+        try {
+            res.render("details/finance")
+        } catch (e) {
+            res.status(404).json({ error: e })
+        }
+    })
+
+    app.use("*", (req, res) => {
+        res.status(404).render("details/error", { error: { status: 404, message: "Not found" } });
+    })
 }
 
 module.exports = constructorMethod
